01181df178653a5e7be86bb08038d171e271da04
mylongcode <mylongcode@gmail.com>
2024-03-04 21:45:50 +0500

    create task8 class

---
+++ AlgLabs/Laba2/Task8.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgLabs.Laba2
+{
+    public class Task8
+    {
+
+        public static void Solution()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[][] array = new int[3][];
+            array[0][2] = 1;
+            foreach (var item in array)
+                Console.WriteLine(item);
+
+        }
+    }
+}
