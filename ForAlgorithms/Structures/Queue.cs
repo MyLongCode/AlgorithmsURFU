ec293cc6a9eed328fe4a09dd3077b781869c7865
mylongcode <mylongcode@gmail.com>
2024-03-12 22:04:46 +0500

    add queue

--- ForAlgorithms/Structures/Queue.cs
+++ ForAlgorithms/Structures/Queue.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace ForAlgorithms.Structures
 {
-    public class Queue
+    public class Queue<T>
     {
+        List<T> queue { get; set; }
+
+        public void Clear()
+        {
+            queue = new List<T>();
+        }
+
+        public T Dequeue()
+        {
+            var temp = queue[0];
+            queue.RemoveAt(0);
+            return temp;
+        }
+
+        public void Enqueue(T Item)
+        {
+            queue.Add(Item);
+        }
+
+
     }
 }
