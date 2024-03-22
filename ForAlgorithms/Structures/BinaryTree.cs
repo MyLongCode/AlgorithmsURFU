e49827165f96c8609921e5d47cf022e276ffdd57
mylongcode <mylongcode@gmail.com>
2024-03-22 21:34:33 +0500

    add binary tree structure

---
+++ ForAlgorithms/Structures/BinaryTree.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForAlgorithms.Structures
+{
+    
+    public class BinaryTree
+    {
+        public class Node
+        {
+            public int key;
+            public Node left;
+            public Node right;
+        }
+
+    }
+}
