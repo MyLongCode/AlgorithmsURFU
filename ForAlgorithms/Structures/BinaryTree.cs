ca2837cc910de92b2b9f08d4ca7c4bd06c277440
mylongcode <mylongcode@gmail.com>
2024-03-22 23:20:20 +0500

    add insert bintree

--- ForAlgorithms/Structures/BinaryTree.cs
+++ ForAlgorithms/Structures/BinaryTree.cs
@@ -9,12 +9,35 @@ namespace ForAlgorithms.Structures
     
     public class BinaryTree
     {
+        public Node root;
         public class Node
         {
             public int key;
             public Node left;
             public Node right;
+            
+            public Node(int key)
+            {
+                this.key = key;
+                left = null;
+                right = null;
+            }
+        }
+        public BinaryTree()
+        {
+            root = null;
+        }
+        public void Insert(int key)
+        {
+            Insert(root, key);
         }
 
+        private Node Insert(Node x, int key)
+        {
+            if (root == null) root = new Node(key);
+            if (key < x.key) x.left = Insert(x.left, key);
+            else if (key > x.key) x.right = Insert(x.right, key);
+            return root;
+        }
     }
 }
