29b77d95fd0fc72a4382fb07edb054507b127eaa
mylongcode <mylongcode@gmail.com>
2024-05-02 21:34:24 +0500

    edit solution in task 3

--- AlgLabs/Laba6/Task3.cs
+++ AlgLabs/Laba6/Task3.cs
@@ -8,22 +8,44 @@ namespace AlgLabs.Laba6
 {
     public class Task3
     {
+        public static int number_groups(int[] a, int L, int N)
+        {
+            const short USED = -1;
+            Array.Sort(a);
+            int res = 0;
+
+            for (int i = 0; i < N; ++i)
+            {
+                if (a[i] == USED)
+                {
+                    continue;
+                }
+                ++res;
+                for (int j = i + 1; j < N; ++j)
+                {
+                    if (a[j] == USED)
+                    {
+                        continue;
+                    }
+                    else if (a[i] + 2 * L < a[j])
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        a[j] = USED;
+                    }
+                }
+            }
+            return res;
+        }
         public static void Solution()
         {
             int[] input = Console.ReadLine().Split().Select(t => int.Parse(t)).ToArray();
             int L = input[0];
             int N = input[1];
             int[] numbers = Console.ReadLine().Split().Select(t => int.Parse(t)).ToArray();
-            Array.Sort(numbers);
-            int center = numbers[0];
-            int counter = 0;
-            for (int i = 1; i < N; i++)
-            {
-                if (numbers[i] - center <= L) continue;
-                center = numbers[i];
-                counter++;
-            }
-            Console.WriteLine(counter);
+            Console.WriteLine(number_groups(numbers, L, N));
         }
     }
 }
