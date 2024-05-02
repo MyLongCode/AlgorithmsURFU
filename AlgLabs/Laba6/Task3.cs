e771af24008a9f44d2ef737f169a90af656ad21f
mylongcode <mylongcode@gmail.com>
2024-05-02 20:07:19 +0500

    add solution in task3

---
+++ AlgLabs/Laba6/Task3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgLabs.Laba6
+{
+    public class Task3
+    {
+        public static void Solution()
+        {
+            int[] input = Console.ReadLine().Split().Select(t => int.Parse(t)).ToArray();
+            int L = input[0];
+            int N = input[1];
+            int[] numbers = Console.ReadLine().Split().Select(t => int.Parse(t)).ToArray();
+            Array.Sort(numbers);
+            int center = numbers[0];
+            int counter = 0;
+            for (int i = 1; i < N; i++)
+            {
+                if (numbers[i] - center <= L) continue;
+                center = numbers[i];
+                counter++;
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
