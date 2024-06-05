aa83a1f7f0d052e68f6b46a16365c5ad129e2335
mylongcode <mylongcode@gmail.com>
2024-06-06 00:02:45 +0500

    add solution in task 27

---
+++ LeetCodeTasks/27 Remove Element.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks
+{
+    public class _27
+    {
+		public static int RemoveElement(int[] nums, int val)
+		{
+			var ans = 0;
+			for(var i = 0; i < nums.Length; i++)
+			{
+				if (nums[i] == val)
+				{
+					ans++;
+					nums[i] = -1;
+				}
+			}
+			Array.Sort(nums);
+			nums.Reverse();
+			return ans;
+		}
+
+		public static void Main()
+		{
+			var i = RemoveElement(new int[] { 3, 2, 2, 3 }, 3);
+		}
+	}
+}
--- LeetCodeTasks/88 Array Merge Sorted.cs
+++ LeetCodeTasks/88 Array Merge Sorted.cs
@@ -2,40 +2,23 @@
 {
     public class Main1
     {
-        public static void Merge(int[] nums1, int m, int[] nums2, int n)
+        public static int RemoveElement(int[] nums, int val)
         {
-            if (m == 0) nums1 = nums2;
-            else if (n == 0) return;
-            else
+            var index = 0;
+            for (var i = 0; i < nums.Length; i++)
             {
-                var arr = new int[m + n];
-                int i = 0;
-                int j = 0;
-                int k = 0;
-                while (i < m || j < n)
+                if (nums[i] != val)
                 {
-                    if (nums1[i] < nums2[j] && i < m)
-                    {
-                        arr[k] = nums1[i];
-                        i++;
-                    }
-                    else
-                    {
-                        arr[k] = nums2[j];
-                        j++;
-                    }
-                    k++;
+                    nums[index] = nums[i];
+                    index++;
                 }
-                nums1 = arr;
             }
-            return;
-            
+            return index;
         }
 
         public static void Main()
         {
-            Merge(new int[] { 1, 2, 3, 0, 0, 0 }, 3, new int[] { 2,5,6}, 3);
-            Console.ReadLine();
+            var i = RemoveElement(new int[] { 3, 2, 2, 3 }, 3);
         }
     }
 }
