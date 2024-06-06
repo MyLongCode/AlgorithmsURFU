0a805bd92d9aa54b2c6703fcd80d8b75923bf077
mylongcode <mylongcode@gmail.com>
2024-06-07 00:19:43 +0500

    add new solution in task 88

--- LeetCodeTasks/88 Array Merge Sorted.cs
+++ LeetCodeTasks/88 Array Merge Sorted.cs
@@ -11,9 +11,34 @@
             Array.Sort(nums1);
         }
 
+        public static void Merge1(int[] nums1, int m, int[] nums2, int n)
+        {
+            int cursor1 = m - 1;
+            int cursor2 = n-1;
+            for(int i = nums1.Length - 1; i >= 0 && cursor2 >= 0; i--)
+            {
+                int n1 = int.MinValue;
+                if (cursor1 >= 0) n1 = nums1[cursor1];
+                int n2 = nums2[cursor2];
+
+                if (n1 < n2)
+                {
+                    nums1[i] = n2;
+                    cursor2--;
+                }
+                else
+                {
+                    nums1[i] = n1;
+                    cursor1--;
+                }
+
+            }
+            return;
+        }
+
         public static void Main()
         {
-            Merge(new int[] { 1, 2, 3, 0, 0, 0 }, 0, new int[] { 2,5,6 }, 3);   
+            Merge1(new int[] { 1, 2, 3, 0, 0, 0 }, 0, new int[] { 2,5,6 }, 3);   
         }
     }
 }
