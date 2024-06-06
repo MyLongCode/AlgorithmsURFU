6628923bd4493218929f2a1cbae9ec7298e14714
mylongcode <mylongcode@gmail.com>
2024-06-07 01:15:24 +0500

    add second solution in task 846

--- LeetCodeTasks/846 Hand of Straights.cs
+++ LeetCodeTasks/846 Hand of Straights.cs
@@ -10,34 +10,67 @@ namespace LeetCodeTasks
     {
         public static bool IsNStraightHand(int[] hand, int groupSize)
         {
-            if (hand.Length % groupSize != 0) return false;
-            Array.Sort(hand);
-            bool flag = true;
-                for(int j = 0; j < hand.Length / groupSize; j++)
-                {
-                    int[] nums = new int[groupSize];
-                    for(int i = 0; i < nums.Length; i++)
-                        nums[i] = int.MinValue;
-                    int count = 0;
-                    for (int k = 0; k < hand.Length; k++)
+                if (hand.Length % groupSize != 0) return false;
+                Array.Sort(hand);
+                bool flag = true;
+                    for(int j = 0; j < hand.Length / groupSize; j++)
                     {
-                        if (hand[k] != -1 && (nums.Contains(hand[k] - 1) || nums[0] == int.MinValue) && !nums.Contains(hand[k]))
+                        int[] nums = new int[groupSize];
+                        for(int i = 0; i < nums.Length; i++)
+                            nums[i] = int.MinValue;
+                        int count = 0;
+                        for (int k = 0; k < hand.Length; k++)
                         {
-                            nums[count] = hand[k];
-                            hand[k] = -1;
-                            count++;
+                            if (hand[k] != -1 && (nums.Contains(hand[k] - 1) || nums[0] == int.MinValue) && !nums.Contains(hand[k]))
+                            {
+                                nums[count] = hand[k];
+                                hand[k] = -1;
+                                count++;
+                            }
+                            if (count == groupSize) 
+                                break;
                         }
-                        if (count == groupSize) 
-                            break;
-                    }
                         
                     
-                }
+                    }
 
-            foreach (var item in hand)
-                if (item != -1) flag = false;
-            return flag;
+                foreach (var item in hand)
+                    if (item != -1) flag = false;
+                return flag;
             
+        }
+
+        public static bool SecondSolution(int[] hand, int groupSize)
+        {
+            if (hand.Length % groupSize != 0)
+                return false;
+
+            var hashMap = new Dictionary<int, int>();
+            foreach (var item in hand)
+            {
+                hashMap[item] = hashMap.GetValueOrDefault(item, 0) + 1;
+            }
+            Array.Sort(hand);
+
+            foreach (int num in hand)
+            {
+                if (hashMap[num] > 0)
+                {
+                    for (int i = num + 1; i < num + groupSize; i++)
+                    {
+                        if (!hashMap.ContainsKey(i))
+                            return false;
+                        if (hashMap[i] == 0)
+                            return false;
+
+                        hashMap[i]--;
+                    }
+
+                    hashMap[num]--;
+                }
+            }
+
+            return true;
         }
 
         public static void Main()
