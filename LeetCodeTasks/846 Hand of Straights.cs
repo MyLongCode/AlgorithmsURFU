a1f02b788c977a09f6208dd4619393ed2601e8d8
mylongcode <mylongcode@gmail.com>
2024-06-07 01:03:36 +0500

    add solution in task 846

---
+++ LeetCodeTasks/846 Hand of Straights.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks
+{
+    public class Hand_of_Straights
+    {
+        public static bool IsNStraightHand(int[] hand, int groupSize)
+        {
+            if (hand.Length % groupSize != 0) return false;
+            Array.Sort(hand);
+            bool flag = true;
+                for(int j = 0; j < hand.Length / groupSize; j++)
+                {
+                    int[] nums = new int[groupSize];
+                    for(int i = 0; i < nums.Length; i++)
+                        nums[i] = int.MinValue;
+                    int count = 0;
+                    for (int k = 0; k < hand.Length; k++)
+                    {
+                        if (hand[k] != -1 && (nums.Contains(hand[k] - 1) || nums[0] == int.MinValue) && !nums.Contains(hand[k]))
+                        {
+                            nums[count] = hand[k];
+                            hand[k] = -1;
+                            count++;
+                        }
+                        if (count == groupSize) 
+                            break;
+                    }
+                        
+                    
+                }
+
+            foreach (var item in hand)
+                if (item != -1) flag = false;
+            return flag;
+            
+        }
+
+        public static void Main()
+        {
+            IsNStraightHand(new int[] { 1,2,3,4,5,6,7,8,9 }, 3);
+        }
+    }
+}
