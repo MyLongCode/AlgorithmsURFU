40d29b66c8f86e09dc2ab4371c1abcf375bdad94
mylongcode <mylongcode@gmail.com>
2024-06-07 11:56:52 +0500

    add solution in task 122

---
+++ LeetCodeTasks/122 Best Time to Buy and Sell Stock II.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks
+{
+    public class _122_Best_Time_to_Buy_and_Sell_Stock_II
+    {
+        public static int MaxProfit(int[] prices)
+        {
+            int index = 0;
+            int ans = 0;
+            for (var i = 0; i < prices.Length; i++)
+            {
+                if (prices[i] < prices[index]) index = i;
+                if (prices[i] > prices[index])
+                {
+                    ans += prices[i] - prices[index];
+                    index = i;
+                }
+            }
+            return ans;
+        }
+
+        public static void Main()
+        {
+            MaxProfit(new int[] { 1,2,3,4,5 });
+        }
+    }
+}
