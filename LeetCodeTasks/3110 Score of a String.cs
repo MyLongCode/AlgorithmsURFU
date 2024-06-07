b8a1899b287aed0e6125fdb90914da9bf13794bb
mylongcode <mylongcode@gmail.com>
2024-06-07 10:52:30 +0500

    add solution in task 3110

---
+++ LeetCodeTasks/3110 Score of a String.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks
+{
+    public class Score_of_a_String
+    {
+        public static int ScoreOfString(string s)
+        {
+            int res = 0;
+            for(var i = 0; i < s.Length - 1; i++)
+            {
+                res += Math.Abs(s[i] - s[i+1]);
+            }
+            return res;
+        }
+        public static void Main()
+        {
+            ScoreOfString("zaz");
+        }
+    }
+}
