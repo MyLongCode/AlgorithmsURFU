308a8d8fd5d9cb3f65794ca531adefa4a5163af1
mylongcode <mylongcode@gmail.com>
2024-06-08 12:08:12 +0500

    add solution in task 125

---
+++ LeetCodeTasks/125 Valid Palindrome.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks
+{
+    public class _125_Valid_Palindrome
+    {
+        public bool IsPalindrome(string s)
+        {
+
+        }
+        public static void Main()
+        {
+
+        }
+    }
+}
