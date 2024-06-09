674069190f1cdd4347bc02faf2830f7d71839a93
mylongcode <mylongcode@gmail.com>
2024-06-09 18:13:39 +0500

    fix TE

--- LeetCodeTasks/974 Subarray Sums Divisible by K.cs
+++ LeetCodeTasks/974 Subarray Sums Divisible by K.cs
@@ -11,27 +11,19 @@ namespace LeetCodeTasks
         public static int SubarraysDivByK(int[] nums, int k)
         {
             int ans = 0;
-            int[] summs = new int[nums.Length + 1];
-            for(var i = 1; i < nums.Length + 1; i++)
-                summs[i] = summs[i - 1] + nums[i - 1];
-            for(var i = 0; i < nums.Length + 1; i++)
+            int[] sums = new int[k];
+            sums[0] = 1;
+            int prefix = 0;
+            for(var i = 0; i < nums.Length; i++)
             {
-                for(var j = 0; j < i; j++)
-                {
-                    if ((summs[i] - summs[j]) % k == 0)
-                        ans++;
-                }
+                prefix = (prefix + nums[i]) % k;
+                if (prefix < 0) prefix += k;
+                sums[prefix]++;
             }
+
+            foreach (int item in sums)
+                ans += item * (item - 1) / 2;
             return ans;
-        }
-        public static void PrintSub(int[] nums, int start, int end)
-        {
-            for(var i = start; i <= end; i++)
-            {
-                if (i < nums.Length)
-                    Console.Write(nums[i] + " ");
-            }
-            Console.WriteLine();
         }
         public static void Main()
         {
