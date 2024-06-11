9ca5f73335f45720f2982a2e4a5f7f05a25259c7
mylongcode <mylongcode@gmail.com>
2024-06-12 00:04:09 +0500

    add solution in task 209

--- LeetCodeTasks/209 Minimum Size Subarray Sum.cs
+++ LeetCodeTasks/209 Minimum Size Subarray Sum.cs
@@ -8,5 +8,29 @@ namespace LeetCodeTasks
 {
     internal class _209_Minimum_Size_Subarray_Sum
     {
+        public static int MinSubArrayLen(int target, int[] nums)
+        {
+            int curSum = 0;
+            int res = int.MaxValue;
+            int start = 0;
+            int i = 0;
+            while(i < nums.Length)
+            {
+                curSum += nums[i];
+                while( curSum >= target)
+                {
+                    res = Math.Min(res, i - start + 1);
+                    curSum -= nums[start];
+                    start++;
+                }
+                i++;
+            }
+            if (res == int.MaxValue) return 0;
+            return res;
+        }
+        public static void Main()
+        {
+            MinSubArrayLen(7,new int[] { 2, 3, 1, 2, 4, 3 });
+        }
     }
 }
