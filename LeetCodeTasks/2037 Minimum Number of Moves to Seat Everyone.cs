d6a848f97854e3289926492a6bf9d0a55380151f
mylongcode <mylongcode@gmail.com>
2024-06-13 23:14:55 +0500

    add solution in task 2037

---
+++ LeetCodeTasks/2037 Minimum Number of Moves to Seat Everyone.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks
+{
+    internal class _2037_Minimum_Number_of_Moves_to_Seat_Everyone
+    {
+        public int MinMovesToSeat(int[] seats, int[] students)
+        {
+            Array.Sort(seats);
+            Array.Sort(students);
+            var res = 0;
+            for(var i = 0; i < seats.Length; i++)
+            {
+                res += Math.Abs(students[i] - seats[i]);
+            }
+            return res;
+        }
+    }
+}
