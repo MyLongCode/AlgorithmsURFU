240f076a26a84e4c7d067c27c59c9e4e2b783615
mylongcode <mylongcode@gmail.com>
2024-06-16 22:27:35 +0500

    add solution in task 330

---
+++ LeetCodeTasks/3169 Count Days Without Meetings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks
+{
+    internal class _3169_Count_Days_Without_Meetings
+    {
+    }
+}
---
+++ LeetCodeTasks/330 Patching Array.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks
+{
+    internal class _330_Patching_Array
+    {
+        public int MinPatches(int[] nums, int n)
+        {
+            int patches = 0;
+            long maxSumCovered = 0;
+            int i = 0;
+
+            while (maxSumCovered < n)
+            {
+                if (i < nums.Length && nums[i] <= maxSumCovered + 1)
+                {
+                    maxSumCovered += nums[i];
+                    i++;
+                }
+                else
+                {
+                    maxSumCovered += maxSumCovered + 1;
+                    patches++;
+                }
+            }
+            GC.Collect();
+            return patches;
+        }
+    }
+}
