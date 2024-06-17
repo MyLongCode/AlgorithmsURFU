672a9db1a2e296589dfe9178a133df9be9425268
mylongcode <mylongcode@gmail.com>
2024-06-17 23:36:11 +0500

    add solution in task 633

--- LeetCodeTasks/633 Sum of Square Numbers.cs
+++ LeetCodeTasks/633 Sum of Square Numbers.cs
@@ -8,5 +8,17 @@ namespace LeetCodeTasks
 {
     internal class _633_Sum_of_Square_Numbers
     {
+        public bool JudgeSquareSum(int c)
+        {
+            int start = 0;
+            int end = (int)(Math.Round(Math.Sqrt(c) + 1) - 1);
+            while(start <= end)
+            {
+                if (start * start + end * end == c) return true;
+                if (start * start + end * end > c) end--;
+                else start++;
+            }
+            return false;
+        }
     }
 }
