788375c9e34a7b116dd754e425d83dc3e7d82be1
mylongcode <mylongcode@gmail.com>
2024-06-17 11:47:12 +0500

    add solution in task 205

---
+++ LeetCodeTasks/205 Isomorphic Strings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks
+{
+    internal class _205_Isomorphic_Strings
+    {
+        public bool IsIsomorphic(string s, string t)
+        {
+            Dictionary<char,char> map = new Dictionary<char,char>();
+            for(var i = 0; i < s.Length; i++)
+            {
+                if ((map.ContainsKey(s[i]) && map[s[i]] != t[i]) 
+                    || (!map.ContainsKey(s[i]) && map.ContainsValue(t[i]))) return false;
+                if (!map.ContainsKey(s[i])) map.Add(s[i], t[i]);
+            }
+            return true;
+        }
+    }
+}
+
