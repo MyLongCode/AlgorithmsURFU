39f3a0d1cfb196af4f5fcea1362a77870d667416
mylongcode <mylongcode@gmail.com>
2024-09-03 22:52:04 +0500

    add solution in task 242

---
+++ LeetCodeTasks/242 Valid Anagram.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks
+{
+    public class _242_Valid_Anagram
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            for(var i = 0; i < s.Length; i++)
+            {
+                if (!dict.ContainsKey(s[i])) dict.Add(s[i], 1);
+                else dict[s[i]]++;
+            }
+            for(var i = 0; i < t.Length; i++)
+            {
+                if (!dict.ContainsKey(t[i])) dict.Add(t[i], -1);
+                else dict[t[i]]--;
+            }
+            foreach (KeyValuePair<char, int> entry in dict)
+            {
+                if (entry.Value != 0) return false;
+            }
+            return true;
+        }
+
+        public static void Main()
+        {
+
+        }
+    }
+}
