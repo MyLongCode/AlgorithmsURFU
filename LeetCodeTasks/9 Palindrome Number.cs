fc3e98d97e83c07b2f563025c6d6b510c1f829ed
mylongcode <mylongcode@gmail.com>
2024-09-12 21:55:50 +0500

    add 9 task

---
+++ LeetCodeTasks/9 Palindrome Number.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks
+{
+    internal class _9_Palindrome_Number
+    {
+        public bool IsPalindrome(int x)
+        {
+
+        }
+    }
+}
