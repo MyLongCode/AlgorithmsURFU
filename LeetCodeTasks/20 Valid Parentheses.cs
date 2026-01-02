6ae0dc8ad9f3fc063969bff31fba5fd38aca3479
Данил Михайлов <mylongcode@gmail.com>
2026-01-02 23:40:29 +0500

    add solution task 20

---
+++ LeetCodeTasks/20 Valid Parentheses.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks
+{
+    internal class _20_Valid_Parentheses
+    {
+        public bool IsValid(string s)
+        {
+            var stack = new Stack<char>();
+            foreach (char c in s)
+            {
+                if (c == '(') { stack.Push(')'); continue; }
+                if (c == '{') { stack.Push('}'); continue; }
+                if (c == '[') { stack.Push(']'); continue; }
+                if (stack.Count == 0 || c != stack.Pop()) return false;
+            }
+            return true;
+
+    }
+}
