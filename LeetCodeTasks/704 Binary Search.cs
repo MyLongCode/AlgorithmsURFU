05286e614e5bd78f359f4e6689373f504209e29d
Данил Михайлов <mylongcode@gmail.com>
2026-01-03 21:37:33 +0500

    add task 20

---
+++ LeetCodeTasks/704 Binary Search.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks
+{
+    internal class _704_Binary_Search
+    {
+        public static int Search(int[] nums, int target)
+        {
+            var left = 0;
+            var right = nums.Length - 1;
+            int point;
+            while(left <= right)
+            {
+                point = left + (right - left) / 2;
+                if (nums[point] == target) return point;
+                if (nums[point] < target) left = point + 1;
+                else right = point - 1;
+                point = (left + right) / 2;
+            }
+            return -1;
+
+
+            // 1 2 4 5 6 7 8 #3
+        }
+
+        public static void Main (string[] args)
+        {
+            Search(new int[] { -1, 0, 3, 5, 9, 12 }, 9);
+        }
+    }
+}
